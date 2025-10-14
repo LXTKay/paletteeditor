f52938e98c8d98daa1431d885773e06dd31c0115
lxtk <thealextk@gmail.com>
2025-10-14 20:37:16 +0200

    fixed colour picker overflow

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,7 +1,18 @@
 import { SketchPicker } from "react-color";
 import "./colorPicker.css";
+import { useLayoutEffect } from "react";
 
 export default function ColorPicker({ hexColour, setPaletteColour }) {
+
+  useLayoutEffect(() => {
+    const colorPicker = document.querySelector(".colorPicker");
+    const offsetTop = colorPicker.getBoundingClientRect().top;
+
+    if (offsetTop < 0) {
+      colorPicker.classList.add("colorPickerShifted");
+    }
+  },);
+
   return (
     <div className="colorPicker" >
       <SketchPicker
